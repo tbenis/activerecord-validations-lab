72017fd19151d85d220db98b393e7d8a136715ad
Benis Tambe <will.jenifer@yahoo.com>
2021-06-10 13:42:28 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ActiveRecord::Base
-    validates :name, presesnce: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true
     validates :phone_number, length: { in: 10 }
 end
